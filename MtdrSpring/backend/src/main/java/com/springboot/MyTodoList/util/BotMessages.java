46d40c8c11735933f2543d095b9c45206ae95064
Facundo Marroquín <a01383541@tec.mx>
2024-11-05 09:51:05 -0600

    Cambios en bot messages

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
@@ -2,13 +2,13 @@ package com.springboot.MyTodoList.util;
 
 public enum BotMessages {
   
-	HELLO_MYTODO_BOT("Hello! I'm ChocoBot!\nType a new todo item below and press the send button (blue arrow), or select an option below:"),
+	HELLO_MYTODO_BOT("Hello! I'm ChocoBot!\nType a new todo ChocoItem below and press the send button (blue arrow), or select an option below:"),
 	BOT_REGISTERED_STARTED("Bot registered and started succesfully!"),
-	ITEM_DONE("Item done! Select /todolist to return to the list of todo items, or /start to go to the main screen."), 
-	ITEM_UNDONE("Item undone! Select /todolist to return to the list of todo items, or /start to go to the main screen."), 
-	ITEM_DELETED("Item deleted! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
-	TYPE_NEW_TODO_ITEM("Type a new todo item below and press the send button (blue arrow) on the right-hand side."),
-	NEW_ITEM_ADDED("New item added! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
+	ITEM_DONE("ChocoItem done! Select /todolist to return to the list of todo ChocoItems, or /start to go to the main screen."), 
+	ITEM_UNDONE("ChocoItem undone! Select /todolist to return to the list of todo ChocoItems, or /start to go to the main screen."), 
+	ITEM_DELETED("ChocoItem deleted! Select /todolist to return to the list of todo ChocoItems, or /start to go to the main screen."),
+	TYPE_NEW_TODO_ITEM("Type a new todo ChocoItem below and press the send button (blue arrow) on the right-hand side."),
+	NEW_ITEM_ADDED("New ChocoItem added! Select /todolist to return to the list of todo ChocoItems, or /start to go to the main screen."),
 	GRAPHICS("Graphics command"), 
 	ITEM_EDIT("Edit task"), 
 	STORYPOINTS("How many storypoints?"),
